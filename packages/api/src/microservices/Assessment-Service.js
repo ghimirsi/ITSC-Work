008ffbe771158505ce702ac58b917788c6420e85
Somyani Ghimire <112344638+ghimirsi@users.noreply.github.com>
2023-01-09 17:00:39 -0500

    added seqeulize . create() in #7 task1

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -1,9 +1,15 @@
 const { Assessment } = require(`../database/models`);
 
-exports.submit = async (assessment) => {
+exports.submit = async (assessment) =>
   // use the sequelize model Assessments from packages/api/src/database/models to save
   // the assessment data in the PostgreSQL database
-};
+  await Assessment.create({
+    catDateOfBirth: assessment.birthDate,
+    catName: assessment.catName,
+    instrumentType: 1,
+    riskLevel: assessment.riskLevel,
+    score: assessment.points,
+  });
 
 exports.getList = () => {
   // use the sequelize model Assessments from packages/api/src/database/models to fetch
