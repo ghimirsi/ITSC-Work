66f78d47f3af3b8348038c5e78c3185c61122f21
Somyani Ghimire <112344638+ghimirsi@users.noreply.github.com>
2023-01-13 16:45:31 -0500

    added extra page called login in system

--- packages/client/src/App.jsx
+++ packages/client/src/App.jsx
@@ -5,6 +5,7 @@ import { DashboardBulletin } from './pages/Dashboard/DashboardBulletin';
 import { NewAssessment } from './pages/Assessments/NewAssessment.jsx';
 import { AssessmentList } from './pages/Assessments/AssessmentList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { LoginPage } from './pages/Assessments/LoginPage';
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,11 @@ const router = createBrowserRouter([
     element: <AssessmentList />,
     path: `/assessment/list`,
   },
+  {
+    element: <LoginPage />,
+    path: `/login`,
+  },
+
 ]);
 
 const App = () => <SiteWrapper>
